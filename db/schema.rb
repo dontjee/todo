638530f23a731642e95eeea70414627cc9e9d5a6
dontjee <dontjee@gmail.com>
2011-01-01 21:09:28 -0500

    Adding password to user model and a basic user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110102011543) do
+ActiveRecord::Schema.define(:version => 20110102015213) do
 
   create_table "items", :force => true do |t|
     t.string   "text"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(:version => 20110102011543) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

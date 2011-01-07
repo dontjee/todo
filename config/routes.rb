ece39064543de4e6522287d39b647630f418a43d
dontjee <dontjee@gmail.com>
2011-01-06 22:18:36 -0500

    Added header links to sign in and out. Created 'pages' controller which is awesome. Added 'home' page to the controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Todo::Application.routes.draw do
+  get "pages/home"
+
   #get "sessions/new"
 
   #get "users/new"
@@ -60,7 +62,7 @@ Todo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "items#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 

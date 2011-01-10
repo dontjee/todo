68dc2ed39ae89943041ff31349070e35ec264751
dontjee <dontjee@gmail.com>
2011-01-09 20:00:53 -0500

    Added authentication of user edit pages and only allow the user to edit their own page. Also updated error messages template to allow it to show errors for any object.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -31,24 +31,24 @@ describe UsersController do
       it "should clear the password field" do
         @attr[:password] = "test"
         post :create, :user => @attr
-        
+
         assigns(:user).password.should == ""
       end
 
       it "should clear the confirmation password field" do
         @attr[:password_confirmation] = "test"
         post :create, :user => @attr
-        
+
         assigns(:user).password_confirmation.should == ""
       end
-      
+
     end
 
     describe "success" do
 
       before(:each) do
         @attr = { :name => "New User", :email => "user@example.com", 
-                  :password => "foobar", :password_confirmation => "foobar" }
+          :password => "foobar", :password_confirmation => "foobar" }
       end
 
       it "should create a user" do
@@ -73,5 +73,114 @@ describe UsersController do
       end
 
     end
+  end
+
+  describe "Get 'edit'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit user")
+    end
+
+  end
+
+  describe "PUT 'update'" do
+
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :email => "", :name => "", :password => "",
+          :password_confirmation => "" }
+      end
+
+      it "should render the 'edit' page" do
+        put :update, :id => @user, :user => @attr
+        response.should render_template('edit')
+      end
+
+      it "should have the right title" do
+        put :update, :id => @user, :user => @attr
+        response.should have_selector("title", :content => "Edit user")
+      end
+    end
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New Name", :email => "user@example.org",
+          :password => "barbaz", :password_confirmation => "barbaz" }
+      end
+
+      it "should change the user's attributes" do
+        put :update, :id => @user, :user => @attr
+        @user.reload
+        @user.name.should  == @attr[:name]
+        @user.email.should == @attr[:email]
+      end
+
+      it "should redirect to the user show page" do
+        put :update, :id => @user, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+
+      it "should have a flash message" do
+        put :update, :id => @user, :user => @attr
+        flash[:success].should =~ /updated/
+      end
+    end
+  end
+
+  describe "authentication of edit/update pages" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    describe "for non-signed in users" do
+
+      it "should deny access to 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(signin_path)
+      end
+
+      it "should deny acces to 'update'" do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(signin_path)
+      end
+
+    end
+
+    describe "for signed in users" do
+      
+      before(:each) do
+        wrong_user = Factory(:user, :email => "wronguser@email.com")
+        test_sign_in(wrong_user)
+      end
+
+      it "should require matching users for 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(root_path)
+      end
+
+      it "should require matching users for 'update'" do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(root_path)
+      end
+
+    end
+
   end
 end
